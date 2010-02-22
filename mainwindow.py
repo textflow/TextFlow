9060a7f7cba6fe09019d70d6862835179ca433a5
Yuri Malheiros <yurimalheiros@gmail.com>
2010-02-22 14:35:02 -0300

    mistake created file deleted

--- mainwindow.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'mainwindow.ui'
-#
-# Created: Mon Feb 22 14:12:03 2010
-#      by: PySide uic UI code generator
-#
-# WARNING! All changes made in this file will be lost!
-

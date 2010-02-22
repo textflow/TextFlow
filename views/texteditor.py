90a7f0e5e437cecb7eece4f0b15342a1faacb5a8
Yuri Malheiros <yurimalheiros@gmail.com>
2010-02-23 01:33:59 +0800

    text editor view created. fix issue #12

---
+++ views/texteditor.py
@@ -0,0 +1,13 @@
+from PySide import QtGui
+
+class TextEditor(QtGui.QTextEdit):
+    """
+    Text editor view.
+    """    
+    
+    def __init__(self, parent):
+        super(TextEditor, self).__init__(parent)
+        self.setAcceptDrops(True)
+        
+    def dropEvent(self, event):
+        print "dropped"

27a25e40a1411bb1d9314670bfc751c25d2ee686
Yuri Malheiros <yurimalheiros@gmail.com>
2010-02-27 10:52:34 +0800

    line numbers performance very improved.

--- views/texteditor.py
+++ views/texteditor.py
@@ -2,6 +2,8 @@ from PyQt4 import QtGui
 from PyQt4.QtCore import QSize, Qt
 from controllers.texteditor import TextEditorController
 
+import math
+
 class TFEditor(QtGui.QWidget):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
@@ -39,41 +41,66 @@ class TextEditor(QtGui.QTextEdit):
                                        filepath, QtGui.QMessageBox.Ok)
                                        
 class LineNumbers(QtGui.QWidget):
+    #TODO
+    # 1 - auto resize (x)
+    # 2 - melhor mais o desempenho (fazer o for apenas entre as linhas que aparecerao) (x)
+    # 3 - refactoring
+    # 4 - bug com blocos com mais de uma linha
+    
     def __init__(self, text_editor, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.text_editor = text_editor
         self.text_editor.installEventFilter(self)
+        self.back_brush = QtGui.QBrush(QtGui.QColor("#d8e0f9"))
     
     def paintEvent(self, event):
         # paint background
-        back_brush = QtGui.QBrush(QtGui.QColor("#6699FF"))
+        
         painter = QtGui.QPainter(self)
-        painter.fillRect(event.rect(), back_brush)
+        painter.fillRect(event.rect(), self.back_brush)
+        
+        document = self.text_editor.document()        
+        
+        contents_y = self.text_editor.verticalScrollBar().value()
+        text_editor_height = self.text_editor.height()
+        line_height = document.documentLayout().blockBoundingRect(document.firstBlock()).height()
+        
+        top_line = int(math.ceil(contents_y / line_height))
+        total_lines = int((text_editor_height / line_height))
         
         
-        document = self.text_editor.document()
-        block = document.begin()
+        pen = QtGui.QPen(QtGui.QColor("#000000"))
+        painter.setPen(pen)
+        painter.setFont(painter.font())
+
         
-        line_number = 1
-        while block.isValid():
+        if document.blockCount() < total_lines:
+            end_range_line = document.blockCount()
+        else:
+            end_range_line = top_line + total_lines
+        
+        
+        for line in range(max(1, top_line), end_range_line + 2):
+            
+            if not line in range(1, document.blockCount() + 1):
+                continue
+            
+            block = document.findBlockByNumber(line - 1)
             position = document.documentLayout().blockBoundingRect(block).topLeft()
-            
-            #write a line
-            pen = QtGui.QPen(QtGui.QColor("#000000"))
-            painter.setPen(pen)
-            painter.setFont(painter.font())
-            align_right = Qt.AlignRight
-            painter.drawText(0, position.y(), self.width()-6, 30,
-                             align_right, str(line_number))
-                             
-            
-            block = block.next()
-            line_number += 1
-            
+#            
+            painter.drawText(self.width() - self.fontMetrics().width(str(line)) - 6, position.y() - contents_y + self.fontMetrics().ascent(), str(line))
+        
         painter.end()
                 
-    def sizeHint(self):
-        return QSize(50, 0)
+    
+    def update(self, *args):
+        document = self.text_editor.document()
+        width = self.fontMetrics().width(str(document.blockCount())) + 12
+        
+        if self.width() != width:
+            self.setFixedWidth(width)
+            
+        QtGui.QWidget.update(self, *args)
     
     def eventFilter(self, object, event):
         # Update the line numbers for all events on the text edit and the viewport.
